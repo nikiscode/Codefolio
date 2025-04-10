54176ddbc598ce61891bc93f44b83a7b326b68b8
Nikitha N <nikitha.nrb@gmail.com>
2025-04-10 21:34:00 +0530

    Update swapping_of_num.c

--- C&CPP/swapping_of_num.c
+++ C&CPP/swapping_of_num.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter num1");
+    scanf("%d",&a);
+    printf("Enter num2");
+    sacnf("%d",&b);
+    c=a;
+    a=b;
+    b=c;
+    printf("%d %d",a,b);
+    return 0;
+}
 
