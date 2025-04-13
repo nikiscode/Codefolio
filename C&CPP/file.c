8a69f108a64cf53b07afa87119acfc5b10fcbbc9
Nikitha N <nikitha.nrb@gmail.com>
2025-04-13 21:10:58 +0530

    Create file.c

---
+++ C&CPP/file.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    FILE *fptr
+    fptr=fopen("C:\\Users\\91789\\Documents\\niki.txt" ,"w");
+    fprintf(fptr,"This is C programming class");
+    fclose(fptr);
+return 0;
+}
