d94bb06f88bd81abcf854215a2c5da444825a9cc
Nikitha N <nikitha.nrb@gmail.com>
2025-04-14 18:41:09 +0530

    Update file.c

--- C&CPP/file.c
+++ C&CPP/file.c
@@ -1,3 +1,4 @@
+/*to create and write something in file*/
 #include <stdio.h>
 int main() {
     FILE *fptr
@@ -6,3 +7,20 @@ int main() {
     fclose(fptr);
 return 0;
 }
+/*to read the file*/
+#include <stdio.h>
+int main() {
+    FILE *fptr;
+    fptr=fopen("C:\\Users\\91789\\Documents\\nikitha.txt" ,"r");
+    fprintf(fptr,"This is C programming class");
+    fprintf(fptr,"\nfirst program");
+    char mystring[1000];
+    fgets(mystring,1000,fptr);
+    printf("%s",mystring);
+    while(fgets(mystring,1000,fptr)){
+        printf("%s",mystring);
+    }
+    fclose(fptr);
+return 0;
+}
+
