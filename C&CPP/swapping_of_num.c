14a290732700160300ab889ffae7b7120a6c0ab7
Nikitha N <nikitha.nrb@gmail.com>
2025-04-19 21:24:47 +0530

    Update swapping_of_num.c

--- C&CPP/swapping_of_num.c
+++ C&CPP/swapping_of_num.c
@@ -11,4 +11,23 @@ int main(){
     printf("%d %d",n,s);
     return 0;
 }
+/*swapping of numbers in type */
+#include<stdio.h>
+typedef int integer;
+int main() {
+    integer s;
+    integer d;
+    integer x;
+    printf("enter the num1:");
+    scanf("%d",&s);
+    printf("enter the num2:");
+    scanf("%d",&d);
+        x=s;
+        s=d;
+        d=x;
+        printf("%d %d",s,d);
+        return 0;
+
+}
+
 
