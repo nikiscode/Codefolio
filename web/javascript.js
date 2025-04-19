42cf199b2df335387a6a30a11ac50742afdfb3fd
Nikitha N <nikitha.nrb@gmail.com>
2025-04-19 21:26:40 +0530

    Update javascript.js

--- web/javascript.js
+++ web/javascript.js
@@ -1,1 +1,10 @@
-
+<!DOCTYPE html>
+<html>
+<body>
+<h2>My First javaScript</h2>
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date and Time.</button>
+<p id="demo"></p>
+</body>
+</html>
