e096b2a0495252b3ad50289cbeb8fa127244f6b3
Nikitha N <nikitha.nrb@gmail.com>
2025-04-22 21:40:26 +0530

    Update file.c

--- C&CPP/file.c
+++ C&CPP/file.c
@@ -3,7 +3,7 @@
 int main() {
     FILE *fptr
     fptr=fopen("C:\\Users\\91789\\Documents\\niki.txt" ,"w");
-    fprintf(fptr,"This is my c file creation");
+    fprintf(fptr,"This is my first file creation in C ");
     fclose(fptr);
 return 0;
 }
@@ -12,7 +12,7 @@ return 0;
 int main() {
     FILE *fptr;
     fptr=fopen("C:\\Users\\91789\\Documents\\nikitha.txt" ,"r");
-    fprintf(fptr,"This is C programming class");
+    fprintf(fptr,"very excited to this program");
     fprintf(fptr,"\nfirst program");
     char mystring[1000];
     fgets(mystring,1000,fptr);
