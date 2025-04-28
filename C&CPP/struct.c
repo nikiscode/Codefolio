e5d6cfa44deb37d1dee596a3d22050f03832ae24
Nikitha N <nikitha.nrb@gmail.com>
2025-04-28 21:17:59 +0530

    Update struct.c

--- C&CPP/struct.c
+++ C&CPP/struct.c
@@ -1,3 +1,4 @@
+/*book title, author , price */
 #include <stdio.h>
 struct book{
 char title[50];
@@ -19,3 +20,23 @@ int main(){
 return 0;
 }
 
+/*dmart itms */
+#include <stdio.h>
+struct item{
+char names[5];
+int quantity[5];
+float cost;
+};
+int main(){
+     struct items i1;
+     printf("enter the name:\n");
+    scanf("%s",i1.name);
+     printf("enter the quantity:\n");
+    scanf("%d",i1.quantity);
+    printf("enter the cost:\n");
+    scanf("%f",i1.cost);
+    printf("name: %s\n",i1.name);
+    printf(" quantity: %s\n",i1. quantity);
+    printf("cost: %f\n",i1.cost);
+return 0;
+}
