815c2019466601d8928ffad8a6a66d5a647df89c
Nikitha N <nikitha.nrb@gmail.com>
2025-04-28 20:49:04 +0530

    Update struct.c

--- C&CPP/struct.c
+++ C&CPP/struct.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+struct book{
+char title[50];
+char author[50];
+float price;
+};
+int main(){
+     struct book b1;
+     printf("enter the title:\n");
+    scanf("%s",b1.title);
+    printf("enter the author:\n");
+    scanf("%s",b1.author);
+     printf("enter the price:\n");
+    scanf("%f",b1.price);
+    printf("title: %s\n",b1.title);
+    printf("author: %s\n",b1.author);
+    printf("price: %f\n",b1.price);
+
+return 0;
+}
 
