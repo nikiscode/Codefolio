2dfb7606f6bdf05f6ba83e47afe8bb286ed48028
Nikitha N <nikitha.nrb@gmail.com>
2025-04-29 09:26:41 +0530

    Update swapping_of_num.c

--- C&CPP/swapping_of_num.c
+++ C&CPP/swapping_of_num.c
@@ -28,5 +28,29 @@ int main() {
     printf("%d %d",s,n);
     return 0;
 }
+/*swaping elements using call by reference*/
+#include <stdio.h>
+void swap(int *a,int *b){
+int temp=*a;
+*a=*b;
+*b=temp;
+}
+int main(){
+int x,y;
+printf("enter first number:");
+scanf("%d",&x);
+
+printf("enter first number:");
+scanf("%d",&y);
+
+printf("before swapping:x=%d, y=%d\n",x,y);
+
+printf("after swapping:x=%d, y=%d\n",x,y);
+
+return 0;
+
+
+}
+
 
 
