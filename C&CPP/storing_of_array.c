0a78ef00ac0f45d2cf787b09ddcc322bb4aaae70
Nikitha N <nikitha.nrb@gmail.com>
2025-04-30 22:08:19 +0530

    Update storing_of_array.c

--- C&CPP/storing_of_array.c
+++ C&CPP/storing_of_array.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 int main() {
+   
     int n, temp;
+  
     printf("Enter number of elements: ");
     scanf("%d", &n);
+    
     int arr[n];
     int *ptr = arr;
+    
     printf("Enter %d elements:\n", n);
     for(int i = 0; i < n; i++) {
         scanf("%d", (ptr + i));
     }
+    
     for(int i = 0; i < n - 1; i++) {
         for(int j = i + 1; j < n; j++) {
             if (*(ptr + i) > *(ptr + j)) {
@@ -18,10 +23,12 @@ int main() {
             }
         }
     }
+    
     printf("Sorted array in ascending order:\n");
     for(int i = 0; i < n; i++) {
         printf("%d ", *(ptr + i));
     }
+    
     return 0;
 }
 
