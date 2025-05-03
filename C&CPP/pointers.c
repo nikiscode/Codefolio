e353e88dec36a5fb0466bec78418f7c49a2637de
Nikitha N <nikitha.nrb@gmail.com>
2025-05-03 22:07:16 +0530

    Update pointers.c

--- C&CPP/pointers.c
+++ C&CPP/pointers.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main() {
-    int arr[4];
+    int arr[8];
     int *ptr = arr;
-    printf("Enter 4 elements:\n");
-    for (int i = 0; i < 4; i++) {
+    printf("Enter 8 elements:\n");
+    for (int i = 0; i < 8; i++) {
         scanf("%d", ptr + i);
     }
     printf("Array elements are:\n");
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 8; i++) {
         printf("%d ", *(ptr + i));
     }
     return 0;
