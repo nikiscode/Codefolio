14051a0728fe0442996e6e3af1ffcd7f8c6d2890
Nikitha N <nikitha.nrb@gmail.com>
2025-05-19 21:59:01 +0530

    Update patterns.c

--- C&CPP/patterns.c
+++ C&CPP/patterns.c
@@ -1,11 +1,17 @@
-// patterns in c programming language //
-#include <stdio.h>
-int main(){
-  int a;
-  int i<1, i==0,i++;
-  printf(*);
-  if i==0;
-  else 
-  
-return 0;
+// patterns in c programming language //#include <stdio.h>
+
+int main() {
+    int i, j, rows;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    for(i = 1; i <= rows; i++) {
+        for(j = 1; j <= i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
