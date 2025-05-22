b504a51da8df923f42625c6389da3d5bdf82a2bd
Nikitha N <nikitha.nrb@gmail.com>
2025-05-22 21:28:00 +0530

    Update file.c

--- C&CPP/file.c
+++ C&CPP/file.c
@@ -1,5 +1,4 @@
 /*to create and write something in file*/
-
 #include <stdio.h>
 int main() {
 FILE *fptr
@@ -8,9 +7,7 @@ fprintf(fptr,"This is my first file creation in C ");
 fclose(fptr);
 return 0;
 }
-
 /*to read the file*/
-
 #include <stdio.h>
 int main() {
     FILE *fptr;
