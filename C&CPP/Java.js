aba1fbb37bb01c2420bd5001f98f10628fd6062d
Nikitha N <nikitha.nrb@gmail.com>
2025-05-27 22:40:22 +0530

    Update Java.js

--- C&CPP/Java.js
+++ C&CPP/Java.js
@@ -27,3 +27,50 @@ for (let i = 1; i <= num; i++) {
 // Display the total count of even and odd numbers
 console.log("Total even numbers: " + evenCount);
 console.log("Total odd numbers: " + oddCount);
+// Ask the user to enter a number
+let num = parseInt(prompt("Enter a number:"));
+
+// Loop from 1 to the entered number
+for (let i = 1; i <= num; i++) {
+    // Check if the number is even
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}// Ask the user to enter a number
+let num = parseInt(prompt("Enter a number:"));
+
+// Loop from 1 to the entered number
+for (let i = 1; i <= num; i++) {
+    // Check if the number is odd
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
+// Ask the user to enter a number
+let num = parseInt(prompt("Enter a number:"));
+// Ask the user to enter a number
+let num = parseInt(prompt("Enter a number:"));
+
+let evenSum = 0;
+let oddSum = 0;
+
+// Loop through numbers from 1 to the entered number
+for (let i = 1; i <= num; i++) {
+    if (i % 2 === 0) {
+        evenSum += i;
+    } else {
+        oddSum += i;
+    }
+}
+
+// Display the results
+console.log("Sum of even numbers: " + evenSum);
+console.log("Sum of odd numbers: " + oddSum);
+
+// Loop from 1 to the entered number
+for (let i = 1; i <= num; i++) {
+    // Check if the number is odd
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
