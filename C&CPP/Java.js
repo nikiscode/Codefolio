40aedb1a233299fc8156c50eb05e46c05f63310b
Nikitha N <nikitha.nrb@gmail.com>
2025-05-27 22:16:23 +0530

    Update Java.js

--- C&CPP/Java.js
+++ C&CPP/Java.js
@@ -1,1 +1,11 @@
+// Ask the user to enter a number
+let num = parseInt(prompt("Enter a number:"));
 
+// Loop through numbers from 1 to the entered number
+for (let i = 1; i <= num; i++) {
+    if (i % 2 === 0) {
+        console.log(i + " is Even");
+    } else {
+        console.log(i + " is Odd");
+    }
+}
