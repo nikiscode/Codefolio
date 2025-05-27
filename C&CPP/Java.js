b13d863ec8a3c88f636748b18dae6c7c1894c876
Nikitha N <nikitha.nrb@gmail.com>
2025-05-27 22:17:53 +0530

    Update Java.js

--- C&CPP/Java.js
+++ C&CPP/Java.js
@@ -8,4 +8,22 @@ for (let i = 1; i <= num; i++) {
     } else {
         console.log(i + " is Odd");
     }
+}//odd and even count
+// Ask the user to enter a number
+let num = parseInt(prompt("Enter a number:"));
+
+let evenCount = 0;
+let oddCount = 0;
+
+// Loop from 1 to the entered number
+for (let i = 1; i <= num; i++) {
+    if (i % 2 === 0) {
+        evenCount++;
+    } else {
+        oddCount++;
+    }
 }
+
+// Display the total count of even and odd numbers
+console.log("Total even numbers: " + evenCount);
+console.log("Total odd numbers: " + oddCount);
