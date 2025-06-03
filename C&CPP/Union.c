d49a53a99708989e5d62c6c95d61f463f5229293
Nikitha N <nikitha.nrb@gmail.com>
2025-06-03 20:55:51 +0530

    Update Union.c

--- C&CPP/Union.c
+++ C&CPP/Union.c
@@ -1,27 +1,21 @@
 //union function 
 #include <stdio.h>
-
 // Define a union
 union Data {
     int i;
     float f;
     char str[20];
 };
-
 int main() {
     union Data data;
-
     // Store an integer
     data.i = 10;
     printf("data.i = %d\n", data.i);
-
     // Store a float (overwrites previous data)
     data.f = 220.5;
     printf("data.f = %.1f\n", data.f);
-
     // Store a string (overwrites previous data)
     snprintf(data.str, sizeof(data.str), "Hello");
     printf("data.str = %s\n", data.str);
-
     return 0;
 }
