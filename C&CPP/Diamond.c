538235513cee7ec90262a6263660aebe0ab4a0dc
Nikitha N <nikitha.nrb@gmail.com>
2026-07-01 23:21:44 +0530

    Update Diamond.c

--- C&CPP/Diamond.c
+++ C&CPP/Diamond.c
@@ -1,15 +1,10 @@
-
 //Diamond_problem
-
 #include <stdio.h>
 int main() {
     int i, j, space, rows;
-
     printf("Enter the number of rows for the diamond (half): ");
     scanf("%d", &rows);
-
-    space = rows - 1;
-
+  space = rows - 1;
     // Upper half of the diamond
     for(i = 1; i <= rows; i++) {
         for(j = 1; j <= space; j++)
@@ -19,10 +14,9 @@ int main() {
         printf("\n");
         space--;
     }
+   space = 1;
 
-    space = 1;
-
-    // Lower half of the diamond
+  // Lower half of the diamond
     for(i = 1; i <= rows - 1; i++) {
         for(j = 1; j <= space; j++)
             printf(" ");
@@ -31,7 +25,6 @@ int main() {
         printf("\n");
         space++;
     }
-
-    return 0;
+   return 0;
 }
 
